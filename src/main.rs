be744ef683518053522765c5f7881ff9c715a34d
Phillip Tennen <phillip@axleos.com>
2024-01-22 17:33:49 +0000

    Check in research on some protocols

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,104 @@ use alloc::vec::Vec;
 use core::mem;
 use log::info;
 use uefi::prelude::*;
-use uefi::Result;
+use uefi::{Guid, guid, Result};
 use uefi::proto::console::gop::{BltOp, BltPixel, BltRegion, GraphicsOutput};
+use uefi::proto::media::block::BlockIoProtocol;
 use uefi::proto::rng::Rng;
 use uefi::table::boot::{OpenProtocolAttributes, OpenProtocolParams};
+use uefi::proto::unsafe_protocol;
+
+#[derive(Debug)]
+#[repr(C)]
+pub struct HttpServiceBindingProtocol {
+    /*
+    pub revision: u64,
+    pub media: *const BlockIoMedia,
+    pub reset: unsafe extern "efiapi" fn(this: *mut Self, extended_verification: bool) -> Status,
+    pub read_blocks: unsafe extern "efiapi" fn(
+        this: *const Self,
+        media_id: u32,
+        lba: Lba,
+        buffer_size: usize,
+        buffer: *mut c_void,
+    ) -> Status,
+    pub write_blocks: unsafe extern "efiapi" fn(
+        this: *mut Self,
+        media_id: u32,
+        lba: Lba,
+        buffer_size: usize,
+        buffer: *const c_void,
+    ) -> Status,
+    pub flush_blocks: unsafe extern "efiapi" fn(this: *mut Self) -> Status,
+    */
+}
+
+impl HttpServiceBindingProtocol {
+    pub const GUID: Guid = guid!("bdc8e6af-d9bc-4379-a72a-e0c4e75dae1c");
+}
+
+#[derive(Debug)]
+#[repr(C)]
+pub struct HttpProtocol {
+    pub get_mode_data: u64,
+    pub configure: u64,
+    pub request: u64,
+    pub cancel: u64,
+    pub response: u64,
+    pub poll: u64,
+}
+
+impl HttpProtocol {
+    pub const GUID: Guid = guid!("7A59B29B-910B-4171-8242-A85A0DF25B5B");
+}
+
+
+#[derive(Debug)]
+#[repr(transparent)]
+#[unsafe_protocol(HttpServiceBindingProtocol::GUID)]
+pub struct HttpBinding(HttpServiceBindingProtocol);
+
+#[derive(Debug)]
+#[repr(transparent)]
+#[unsafe_protocol(HttpProtocol::GUID)]
+pub struct Http(HttpProtocol);
+
+#[derive(Debug)]
+#[repr(C)]
+pub struct IPv4Protocol {
+}
+
+impl IPv4Protocol {
+    //pub const GUID: Guid = guid!("00720665-67EB-4a99-BAF7-D3C33A1C7CC9");
+    //pub const GUID: Guid = guid!("65530BC7-A359-410f-B010-5AADC7EC2B62");
+    //pub const GUID: Guid = guid!("41d94cd2-35b6-455a-8258-d4e51334aadd");
+    pub const GUID: Guid = guid!("41d94cd2-35b6-455a-8258-d4e51334aadd");
+}
+#[derive(Debug)]
+#[repr(transparent)]
+#[unsafe_protocol(IPv4Protocol::GUID)]
+pub struct IPv4(IPv4Protocol);
+
+// DNS4: AE3D28CC-E05B-4FA1-A011-7EB55A3F1401 BDB49030
+// UDP4: 3AD9DF29-4501-478D-B1F8-7F7FE70E50F3 BDB49D38
+// IP4: 41D94CD2-35B6-455A-8258-D4E51334AADD BDB496A0
+// TCP4: 65530BC7-A359-410F-B010-5AADC7EC2B62 BDB4CE38
+// HTTP: 7A59B29B-910B-4171-8242-A85A0DF25B5B BDB4C020
+
+#[derive(Debug)]
+#[repr(C)]
+pub struct TCPv4Protocol {
+
+}
+
+impl TCPv4Protocol {
+    pub const GUID: Guid = guid!("65530BC7-A359-410F-B010-5AADC7EC2B62");
+}
+#[derive(Debug)]
+#[repr(transparent)]
+#[unsafe_protocol(TCPv4Protocol::GUID)]
+pub struct TCPv4(TCPv4Protocol);
+
 
 struct Buffer {
     width: usize,
@@ -89,6 +183,26 @@ fn main(_image_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     uefi_services::init(&mut system_table).unwrap();
 
     info!("Hello world!");
-    draw(system_table.boot_services()).unwrap();
+    //draw(system_table.boot_services()).unwrap();
+
+    let bt = system_table.boot_services();
+    //let handle = bt.get_handle_for_protocol::<Http>().unwrap();
+    let handle = bt.get_handle_for_protocol::<TCPv4>();
+    info!("Handle: {handle:?}");
+    /*
+    info!("Got handle {handle:?}");
+    //let mut gop = bt.open_protocol_exclusive::<GraphicsOutput>(gop_handle).unwrap();
+    let h = unsafe {
+        bt.open_protocol::<Http>(
+            OpenProtocolParams {
+                handle,
+                agent: bt.image_handle(),
+                controller: None,
+            },
+            OpenProtocolAttributes::Exclusive,
+        )
+    }.unwrap();
+    info!("Got h {h:?}");
+    */
     Status::SUCCESS
 }
