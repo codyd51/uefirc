d0ebc8569e8a644881467e67d45a3327f554d175
Phillip Tennen <phillip@axleos.com>
2024-01-30 15:02:24 +0000

    TCP client provides some modelling for setting nickname

--- src/app.rs
+++ src/app.rs
@@ -1,3 +1,5 @@
+use alloc::format;
+use alloc::rc::Rc;
 use log::info;
 use uefi::prelude::BootServices;
 use uefi::table::boot::ScopedProtocol;
@@ -8,15 +10,15 @@ use crate::tcpv4::TCPv4ServiceBindingProtocol;
 
 #[derive(Debug)]
 pub struct IrcClient<'a> {
-    boot_services: &'a BootServices,
+    boot_services: &'static BootServices,
     tcp_service_binding_protocol: ScopedProtocol<'a, TCPv4ServiceBindingProtocol>,
 
-    pub active_connection: Option<TcpConnection<'a>>,
+    pub active_connection: Option<Rc<TcpConnection<'a>>>,
 }
 
 impl<'a> IrcClient<'a> {
     pub fn new(
-        boot_services: &'a BootServices,
+        boot_services: &'static BootServices,
     ) -> Self {
         let tcp_service_binding_protocol = get_tcp_service_binding_protocol(boot_services);
 
@@ -27,7 +29,7 @@ impl<'a> IrcClient<'a> {
         }
     }
 
-    fn connect_to_server(&mut self) {
+    pub fn connect_to_server(&mut self) {
         info!("Initializing connection to IRC server...");
         let tcp_protocol = get_tcp_protocol(
             self.boot_services,
@@ -43,11 +45,23 @@ impl<'a> IrcClient<'a> {
         self.active_connection = Some(connection);
     }
 
+    pub fn set_nickname(&mut self, nickname: &str) {
+        let data = format!("NICK {nickname}\r\n").into_bytes();
+        let conn = self.active_connection.as_mut();
+        conn.unwrap().transmit(&data);
+    }
+
+    pub fn set_user(&mut self, nickname: &str, real_name: &str) {
+        let data = format!("/USER {nickname} 0 * :{real_name}\r\n").into_bytes();
+        let conn = self.active_connection.as_mut();
+        conn.unwrap().transmit(&data);
+    }
+
     pub fn step(&mut self) {
         if self.active_connection.is_none() {
             self.connect_to_server();
         }
         let mut connection = self.active_connection.as_mut().expect("We should always be connected to the server now.");
-        connection.step();
+        //connection.step();
     }
 }
