083baff22ea0feb656cfb7eb99617fcbff53bc3c
Phillip Tennen <phillip@axleos.com>
2024-02-03 22:31:29 +0000

    Tokenizer allows reading to any of multiple delimiters

--- src/irc/tokenizer.rs
+++ src/irc/tokenizer.rs
@@ -28,6 +28,36 @@ impl Tokenizer {
         Some(part)
     }
 
+    pub fn read_to_any(&mut self, delimiters: &[&str]) -> Option<String> {
+        let mut earliest_delimiter_pos = None;
+        let mut delimiter_len = 0;
+
+        for &delimiter in delimiters {
+            if let Some(idx) = self.line[self.cursor..].windows(delimiter.len()).position(|w| w == delimiter.chars().collect::<Vec<char>>()) {
+                if earliest_delimiter_pos.map_or(true, |e| idx < e) {
+                    earliest_delimiter_pos = Some(idx);
+                    delimiter_len = delimiter.len();
+                }
+            }
+        }
+
+        match earliest_delimiter_pos {
+            // No delimiter found
+            None => None,
+            Some(delim_start_idx) => {
+                let part = self.line[self.cursor..self.cursor + delim_start_idx].iter().collect::<String>();
+                // Skip the delimiter as well
+                // Update the cursor position to skip past the delimiter
+                self.cursor += delim_start_idx + delimiter_len;
+                Some(part)
+            },
+        }
+    }
+
+    pub fn read_to_str(&mut self, s: &str) -> Option<String> {
+        self.read_to_any(&[s])
+    }
+
     /// Read the remainder of the buffer
     pub fn read(&mut self) -> Option<String> {
         if self.cursor == self.line.len() {
@@ -84,6 +114,25 @@ mod test {
         t.match_str("This");
         assert_eq!(t.peek(), Some(' '));
         assert_eq!(t.read(), Some(" is a test".to_string()));
+    }
+
+    #[test]
+    fn test_read_to_any_delim() {
+        let mut t = Tokenizer::new(&"This is a test\r\n");
+        let delims = [" ", "\r\n"];
+        assert_eq!(t.read_to_any(&delims), Some("This".to_string()));
+        assert_eq!(t.read_to_any(&delims), Some("is".to_string()));
+        assert_eq!(t.read_to_any(&delims), Some("a".to_string()));
+        assert_eq!(t.read_to_any(&delims), Some("test".to_string()));
+        assert_eq!(t.read_to_any(&delims), None);
+    }
+
+    #[test]
+    fn test_read_to_str() {
+        let mut t = Tokenizer::new(&"Line1\r\nLine2\r\n");
+        assert_eq!(t.read_to_str("\r\n"), Some("Line1".to_string()));
+        assert_eq!(t.read_to_str("\r\n"), Some("Line2".to_string()));
+        assert_eq!(t.read_to_str("\r\n"), None);
     }
 }
 
