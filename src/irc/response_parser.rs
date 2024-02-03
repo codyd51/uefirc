3e02c3fbe9b9b961536a6d1831d4955db43acc48
Phillip Tennen <phillip@axleos.com>
2024-02-03 22:39:16 +0000

    Parse Join command

--- src/irc/response_parser.rs
+++ src/irc/response_parser.rs
@@ -3,30 +3,30 @@ use alloc::vec;
 use alloc::vec::Vec;
 use crate::irc::Tokenizer;
 
-#[derive(Debug, Clone)]
+const IRC_LINE_DELIMITER: &'static str = "\r\n";
+
+#[derive(Debug, Clone, PartialEq)]
 pub struct User(String);
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct Channel(String);
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct UserOrChannel(String);
 
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct JoinParameters {
-    user: User,
     channel: Channel,
 }
 
 impl JoinParameters {
-    fn new(user: &User, channel: &Channel) -> Self {
+    fn new(channel: &Channel) -> Self {
         Self {
-            user: user.clone(),
             channel: channel.clone(),
         }
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct PrivateMessageParameters {
     sender: User,
     recipient: UserOrChannel,
@@ -48,6 +48,7 @@ impl PrivateMessageParameters {
 }
 
 #[derive(Debug, Copy, Clone)]
+#[derive(PartialEq)]
 pub enum IrcCommandName {
     Join,
     PrivateMessage,
@@ -65,7 +66,7 @@ impl From<&str> for IrcCommandName {
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum IrcCommand {
     Join(JoinParameters),
     PrivateMessage(PrivateMessageParameters),
@@ -110,8 +111,7 @@ impl ResponseParser {
 
     fn read_next_line(&mut self) -> Option<String> {
         // Check whether we've got a line ready to parse
-        // TODO(PT): Extract the IRC newline sequence?
-        let irc_newline_seq = b"\r\n";
+        let irc_newline_seq = IRC_LINE_DELIMITER.as_bytes();
         let newline_pos = self.buffered_data.windows(2).position(|w| w == irc_newline_seq);
         let newline_start_idx = match newline_pos {
             // No newline ready yet
@@ -139,11 +139,27 @@ impl ResponseParser {
             false => None,
         };
 
+        let raw_command_name = tokenizer.read_to(' ').expect("Failed to read a command");
+        let command_name = IrcCommandName::from(&raw_command_name as &str);
+
+        let command = match command_name {
+            IrcCommandName::Join => {
+                let channel = tokenizer.read_to_str(IRC_LINE_DELIMITER).expect("Failed to read a channel name");
+                if channel.contains(" ") {
+                    // Only clients can specify multiple channels
+                    panic!("Multiple channels mentioned, servers should not send multiple channels?")
+                }
+                IrcCommand::Join(JoinParameters::new(&Channel(channel)))
+            }
+            IrcCommandName::PrivateMessage => todo!(),
+            IrcCommandName::Notice => todo!(),
+        };
+
         Some(
             IrcMessage::new(
                 origin,
-                IrcCommandName::Join,
-                IrcCommand::Join(JoinParameters::new(&User("pt".to_string()), &Channel("test".to_string())))
+                command_name,
+                command,
             )
         )
     }
@@ -152,15 +168,32 @@ impl ResponseParser {
 #[cfg(test)]
 mod test {
     use alloc::string::ToString;
-    use crate::irc::{ResponseParser, Tokenizer};
+    use crate::irc::{ResponseParser};
+    use crate::irc::response_parser::{Channel, IrcCommand, IrcCommandName, JoinParameters};
 
     #[test]
     fn test_parse_welcome_message() {
         let mut p = ResponseParser::new();
-        p.ingest(":irc.example.com 001 nick :Welcome to the IRC Network, YourNick!YourUser@your.host\r\n".as_bytes());
+        p.ingest(":irc.example.com 001 Nick :Welcome to the IRC Network, Nick!Username@Hostname\r\n".as_bytes());
         let parsed_msg = p.parse_next_line().expect("Failed to parse message");
 
         assert_eq!(parsed_msg.origin, Some("irc.example.com".to_string()));
+
+        assert!(p.parse_next_line().is_none());
+    }
+
+    #[test]
+    fn test_parse_multiple_lines() {
+        let mut p = ResponseParser::new();
+        p.ingest("JOIN #chan1\r\nJOIN #chan2\r\n".as_bytes());
+        let msg1 = p.parse_next_line().unwrap();
+        assert_eq!(msg1.origin, None);
+        assert_eq!(msg1.command_name, IrcCommandName::Join);
+        assert_eq!(msg1.command, IrcCommand::Join(JoinParameters::new(&Channel("#chan1".to_string()))));
+        let msg2 = p.parse_next_line().unwrap();
+        assert_eq!(msg2.origin, None);
+        assert_eq!(msg2.command_name, IrcCommandName::Join);
+        assert_eq!(msg2.command, IrcCommand::Join(JoinParameters::new(&Channel("#chan2".to_string()))));
 
         assert!(p.parse_next_line().is_none());
     }
